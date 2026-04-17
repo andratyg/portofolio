debe63c611bc6297b1be602286aa368f5a24fe6c
Andra Indra <indraandra545@gmail.com>
2026-04-17 10:15:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6743233958-ba96d",
+  "appId": "1:198456912343:web:ccb7f1994b798ab6730392",
+  "apiKey": "AIzaSyBKnH4QH594VGGni36XRUmA0DybPeUdyoo",
+  "authDomain": "studio-6743233958-ba96d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "198456912343"
+};
